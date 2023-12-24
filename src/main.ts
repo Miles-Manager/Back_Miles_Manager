e4dff46a1b9980c712beab7086e06148f65d4be9
Bkuste2 <igorribeiro632@gmail.com>
2023-12-24 01:12:05 -0300

    :wrench: 	chore: specify methods, credentials and headers allowed by cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'http://localhost:5173' });
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+    methods: 'GET, POST, PATCH, DELETE, OPTIONS',
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
